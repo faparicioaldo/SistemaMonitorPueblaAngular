d3c1448a50766f7d80abe19391518e836244ab7f
faparicioaldo <faparicioaldo@gmail.com>
2020-08-01 19:05:02 -0500

    Se habilita scheduler

--- src/main/java/com/puebla/monitoralertas/SistemaMonitorApplication.java
+++ src/main/java/com/puebla/monitoralertas/SistemaMonitorApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class SistemaMonitorApplication extends SpringBootServletInitializer {
 
 	@Override
