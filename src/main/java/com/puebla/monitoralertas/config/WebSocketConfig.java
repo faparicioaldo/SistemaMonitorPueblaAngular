ce18e0f400a89b64fd59c70d2cff949d48020820
faparicioaldo <faparicioaldo@gmail.com>
2020-09-19 19:20:29 -0500

    se agregan parametros para que levante la base

--- src/main/java/com/puebla/monitoralertas/config/WebSocketConfig.java
+++ src/main/java/com/puebla/monitoralertas/config/WebSocketConfig.java
@@ -14,6 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
 //        .setAllowedOrigins("*")
+//        .setHandshakeHandler(defaultHandshakeHandler())
         .withSockJS();
     }
 
