4acc5669cf6d05465fd2c7ddb5c36b8af1e421e9
faparicioaldo <faparicioaldo@gmail.com>
2020-10-25 17:24:31 -0600

    Se agrega model mapper para DTO

--- src/main/java/com/puebla/monitoralertas/Application.java
+++ src/main/java/com/puebla/monitoralertas/Application.java
@@ -1,8 +1,10 @@
 package com.puebla.monitoralertas;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -13,4 +15,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
 }
