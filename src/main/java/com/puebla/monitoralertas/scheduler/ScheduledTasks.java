9ccffc8a61d8c8086cd9523b7a63f536c74c9274
faparicioaldo <faparicioaldo@gmail.com>
2020-11-09 19:08:59 -0600

    Se divide scheduler en 3 procesos sincrnizacion de alertas, envio de gps y sincronizacion de vehiculos

--- src/main/java/com/puebla/monitoralertas/scheduler/ScheduledTasks.java
+++ src/main/java/com/puebla/monitoralertas/scheduler/ScheduledTasks.java
@@ -1,13 +1,9 @@
 package com.puebla.monitoralertas.scheduler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.puebla.monitoralertas.config.GlobalSession;
-import com.puebla.monitoralertas.dto.ChatMessage;
-import com.puebla.monitoralertas.dto.ChatMessage.MessageType;
 import com.puebla.monitoralertas.service.CeibaVehiculoService;
 import com.puebla.monitoralertas.service.EnviarAlarmaGobiernoService;
 
@@ -17,7 +13,6 @@ import lombok.extern.log4j.Log4j2;
  * referencia
  * https://www.callicoder.com/spring-boot-task-scheduling-with-scheduled-annotation/
  * */
-
 @Component
 @Log4j2
 public class ScheduledTasks {
@@ -28,53 +23,39 @@ public class ScheduledTasks {
 	@Autowired
 	private CeibaVehiculoService ceibaVehiculoService;
 	
-	@Autowired
-	private GlobalSession session;
-
-	@Autowired
-	private SimpMessagingTemplate template;
-
 	/**
-	 * Proceso automatico para enviar alertas y gps's a semovi.
-	 * 
-	 * @author Aldo Flores
+	 * Proceso automatico para enviar gps's a webmaps.
 	 * 
 	 * */
-	@Scheduled(fixedRate = 15000)
-	public void scheduleTaskWithFixedRate() {
-//		log.info("------------------------------------------------------------------");
-//		log.info("INICIO: scheduler");
-//		log.info("------------------------------------------------------------------");
-		
-//	      template.convertAndSend("/topic/vehicle", mensaje);
-//	      template.convertAndSend("/topic/alert", mensaje);
-//	      template.convertAndSend("/topic/gps", mensaje);
-			
-		//Consulta lista de vehiculos registrados en CEIBA2
-		ceibaVehiculoService.actualizarVehiculosCeibaInMonitor();
-		
-//		enviarAlarmasSemovi.pruebaGPS();
+	@Scheduled(fixedRate = 5000)
+	public void scheduleTaskGpsWithFixedRate() {
+		log.info("------------------------------------------");
+		log.info("SCHEDULER 1: Enviar GPS's (los que cambiaron) a WebMaps");
+		log.info("------------------------------------------");
+		enviarAlarmasSemovi.enviarGPSs();
+	}
+
+	/**
+	 * Proceso automatico para sincronizar alertas con CEIBA
+	 * 
+	 * */
+	@Scheduled(fixedRate = 7000)
+	public void scheduleTaskAlertsWithFixedRate() {
+		log.info("------------------------------------------");
+		log.info("SCHEDULER 2: Sincronizar alertas con CEIBA");
+		log.info("------------------------------------------");
 		enviarAlarmasSemovi.actualizarConAlertasCeiba();
-		
-		//Cuando se cumplan N (CiclosContadorGPS definidos por variable ciclosContadorGPS en GlobalSession) 
-		//se realiza en envio de mensajes (GPS) de todos los vehiculos en CEIBA2 y en Monitor de Alarmas a SEMOVI 
-//		session.setContadorEnviarGPSs(session.getContadorEnviarGPSs() + 1);
-//		if(session.getContadorEnviarGPSs().equals(session.getCiclosContadorGPS())) {
-//			session.setContadorEnviarGPSs(0);//Reinicia contador
-			enviarAlarmasSemovi.enviarGPSs();
-//		}
-		
+	}
+
+	/**
+	 * Proceso automatico para sincronizar con nuevos vehiculos registrados en CEIBA cada 4 horas a partir de la media noche.
+	 * 
+	 * */
+	@Scheduled(cron = "0 0 0/4 ? * * *")
+	public void scheduleTaskVehicleWithCronExpression() {
 		log.info("------------------------------------------------------------------");
-//		log.info("FIN: scheduler");
-//		log.info("------------------------------------------------------------------");
-	}
-
-	public void scheduleTaskWithFixedDelay() {
-	}
-
-	public void scheduleTaskWithInitialDelay() {
-	}
-
-	public void scheduleTaskWithCronExpression() {
+		log.info("SCHEDULER 3: Sincronizar vehiculos con CEIBA");
+		log.info("------------------------------------------------------------------");
+		ceibaVehiculoService.actualizarVehiculosCeibaInMonitor();				
 	}
 }
